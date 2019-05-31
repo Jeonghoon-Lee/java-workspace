4ff51946c670f9ea119959ab4b98d13d5f86eea6
Jeonghoon-Lee <hoon22.lee@gmail.com>
2019-05-30 23:47:25 -0400

    Updated lambda examples

--- WebService/src/assignment1/Main.java
+++ WebService/src/assignment1/Main.java
@@ -11,60 +11,66 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        ArrayList<Person> people = new ArrayList<>();
+        ArrayList<Person> personList = new ArrayList<>();
 
-        people.add(new Person(5, "Marie", "France"));
-        people.add(new Person(2, "Michael", "Jordan"));
-        people.add(new Person(4, "James", "Dean"));
-        people.add(new Person(3, "Michael", "Jackson"));
-        people.add(new Person(1, "Bill", "Smith"));
+        // Initialize ArrayList
+        personList.add(new Person(5, "Marie", "France"));
+        personList.add(new Person(2, "Mark", "Corry"));
+        personList.add(new Person(4, "James", "Dean"));
+        personList.add(new Person(3, "Michael", "Jackson"));
+        personList.add(new Person(1, "Bill", "Smith"));
 
-        Collections.sort(people);
-        printPersonList(people, "\n--- Sort by id using Comparable ---");
+        // Sort using Comparable
+        Collections.sort(personList);
+        printPersonList(personList, "\n--- Sort by id using Comparable ---");
 
         // Using Inner Class
-        Collections.sort(people, new Person.SortByName());
-        printPersonList(people, "\n--- Sort by Nams using Inner Class ---");
+        Collections.sort(personList, new Person.SortByName());
+        printPersonList(personList, "\n--- Sort by Nams using Inner Class ---");
 
-        Collections.sort(people, new Person.SortByFamily());
-        printPersonList(people, "\n--- Sort by Family using Inner Class ---");
+        Collections.sort(personList, new Person.SortByFamily());
+        printPersonList(personList, "\n--- Sort by Family using Inner Class ---");
 
-        Collections.sort(people, new Person.SortById());
-        printPersonList(people, "\n--- Sort by Id using Inner Class ---");
+        Collections.sort(personList, new Person.SortById());
+        printPersonList(personList, "\n--- Sort by Id using Inner Class ---");
 
-        // Using Anonymous Inner Class
-        Collections.sort(people, new Comparator<Person>() {
+        // Using Anonymous Class
+        Collections.sort(personList, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
                 return o1.getName().compareTo(o2.getName());
             }
         });
-        printPersonList(people, "\n--- Sort by Nams using Anonymous Inner Class ---");
+        printPersonList(personList, "\n--- Sort by Nams using Anonymous Inner Class ---");
 
-        Collections.sort(people, new Comparator<Person>() {
+        Collections.sort(personList, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
                 return o1.getFamily().compareTo(o2.getFamily());
             }
         });
-        printPersonList(people, "\n--- Sort by Family using Anonymous Inner Class ---");
+        printPersonList(personList, "\n--- Sort by Family using Anonymous Inner Class ---");
 
-        Collections.sort(people, new Comparator<Person>() {
+        Collections.sort(personList, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
                 return o1.getId() - o2.getId();
             }
         });
-        printPersonList(people, "\n--- Sort by Id using Anonymous Inner Class ---");
+        printPersonList(personList, "\n--- Sort by Id using Anonymous Inner Class ---");
 
         // Using Lambda
-        Collections.sort(people, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-        printPersonList(people, "\n--- Sort by Nams using Lambda ---");
+        Collections.sort(personList, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        // Collections.sort(personList, Comparator.comparing(person -> person.getName()));
+        // Collections.sort(personList, Comparator.comparing(Person::getName));
+        printPersonList(personList, "\n--- Sort by Nams using Lambda ---");
 
-        Collections.sort(people, (o1, o2) -> o1.getFamily().compareTo(o2.getFamily()));
-        printPersonList(people, "\n--- Sort by Family using Lambda ---");
+        // Collections.sort(personList, (o1, o2) -> o1.getFamily().compareTo(o2.getFamily()));
+        Collections.sort(personList, Comparator.comparing(person -> person.getFamily()));
+        printPersonList(personList, "\n--- Sort by Family using Lambda ---");
 
-        Collections.sort(people, (o1, o2) -> o1.getId() - o2.getId());
-        printPersonList(people, "\n--- Sort by Id using Lambda ---");
+        // Collections.sort(personList, (o1, o2) -> o1.getId() - o2.getId());
+        Collections.sort(personList, Comparator.comparingInt(Person::getId));
+        printPersonList(personList, "\n--- Sort by Id using Lambda ---");
     }
 }
